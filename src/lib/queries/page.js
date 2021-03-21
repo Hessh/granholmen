e9b497330134b49d19ca58a27e47626ba4f8702d
Haakon Hess <haakon.hess@gmail.com>
2021-03-22 00:32:39 +0100

    made more queries

--- src/lib/queries/page.js
+++ src/lib/queries/page.js
@@ -24,3 +24,100 @@ export const GET_HOMEPAGE = `
     }
     ${MENU_FRAGMENT}
   }`
+
+export const GET_BOARD_MEMBERS = `
+  query getBoardMembers{
+    page(id: "styret", idType: URI) {
+      slug
+      title
+      content
+      excerpt
+      featuredImage {
+        node {
+          sourceUrl
+        }
+      }
+      styrePage {
+        medlemmer {
+          medlem {
+            ... on Styremedlem {
+              styremedlem {
+                name
+                position
+                phone
+                mail
+                gender
+              }
+            }
+          }
+        }
+      }
+      ${SEO_FRAGMENT}
+    }
+    ${MENU_FRAGMENT}
+  }`
+
+export const GET_REPORTS = `
+  query getReports{
+    page(id: "referat", idType: URI) {
+      slug
+      title
+      content
+      excerpt
+      featuredImage {
+        node {
+          sourceUrl
+        }
+      }
+      referatPage {
+        referater {
+          referat {
+            ... on Referat {
+              referat {
+                name
+                date
+                file {
+                  sourceUrl
+                }
+              }
+            }
+          }
+        }
+      }
+      ${SEO_FRAGMENT}
+    }
+    ${MENU_FRAGMENT}
+  }`
+
+export const GET_ANNUAL_REPORTS = `
+  query getReports{
+    page(id: "årsberetning", idType: URI) {
+      slug
+      title
+      content
+      excerpt
+      featuredImage {
+        node {
+          sourceUrl
+        }
+      }
+      arsberetningPage {
+        arsberetninger {
+          arsberetning {
+            ... on Arsberetning {
+              id
+              arsberetning {
+                name
+                date
+                file {
+                  sourceUrl
+                }
+              }
+            }
+          }
+        }
+      }
+      ${SEO_FRAGMENT}
+    }
+    ${MENU_FRAGMENT}
+  }`
